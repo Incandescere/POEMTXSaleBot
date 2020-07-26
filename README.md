57d307ec9f7c2fd0600b437424a2ff9ba900e2ba
valy <bryan00iwe@gmail.com>
2020-07-26 22:44:53 +0800

    updated readme, can scrape images

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# POEMTXSaleBot
+[POE MTX Sale Bot](https://telegram.me/POEMTXSaleBot)
+
+This telegram bot fetches and displays the microtransaction items on sale daily on the [Path of Exile MTX sale page](https://www.pathofexile.com/shop/category/daily-deals#)
+
+
+
+Made with NodeJS and the following packages:
+
+[Puppeteer](https://github.com/puppeteer/puppeteer): Headless web browsing and scraping 
+
+[Telegraf](https://telegraf.js.org/#/): A NodeJS telegram API
+
+[micro-bot](https://www.npmjs.com/package/micro-bot): Extension of Telegraf compatible with heroku deployment
+
+
+
+Other useful links: 
+
+Puppeteer tutorial: https://blog.bitsrc.io/web-scraping-with-puppeteer-e73e5fee7474
+
+Telegram bot deployment on heroku: https://github.com/ishan0445/telegraf-telegram-bot-deploy
+
+Required puppeteer-heroku buildpack: https://elements.heroku.com/buildpacks/jontewks/puppeteer-heroku-buildpack
+
+Env: 
+
+OS: Ubuntu 18.04.4 LTS
+
+NodeJS: 12.18.3
+
+npm: 6.14.6
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const puppeteer = require("puppeteer");
-//const Telegraf=require('telegraf')
+//const Telegraf=require('telegraf');
 const {Composer}  = require('micro-bot')
 
 //const bot = new Telegraf('1123755502:AAGKCqd36deAKDlj2dz-Am7201q4ZdvdGEs')
-
 const bot = new Composer
 
 const dailyList = async ()=>{
@@ -20,16 +19,22 @@ const dailyList = async ()=>{
     await page.goto(url, {waitUntil: 'networkidle2'});
 
     var nameList = await page.evaluate(()=>{
+        var itemList = []
+        document.querySelectorAll('a.name').forEach(x=>itemList.push(x.innerText.trim()))
+        var priceList = []
+        document.querySelectorAll('div.price').forEach(x=>priceList.push(x.innerText.trim()))
+        // var images = []
+        // document.querySelectorAll('div>a>img.itemImage.replace').forEach(x=>images.push(x.src))
 
-        var list = document.querySelectorAll('a.name')
-        var ret = []
+        var ret = priceList
 
-        for(var i=0; i<list.length;i++){
-            ret[i]=list[i].innerText.trim()
+        for(var i=0; i<itemList.length;i++){
+            ret[i]=itemList[i]+' '+priceList[i]/*+' \n'+images[i]*/;
         }
+
         ret.sort();
 
-        for(var i=0;i<list.length;i++){
+        for(var i=0;i<itemList.length;i++){
             ret[i] = (i+1)+": " +ret[i]
         }
 
@@ -42,10 +47,63 @@ const dailyList = async ()=>{
     //console.log(nameList);
 
     await browser.close();
-    console.log("Queried");
 
-    return nameList.join('\n');
+    return nameList.join('\n')
 };
+
+const imageLinks = async ()=>{
+     // open the headless browser
+    var browser = await puppeteer.launch({ args: ['--no-sandbox'], headless:true });
+
+     // open a new page
+    var page = await browser.newPage();
+ 
+    const url = 'https://www.pathofexile.com/shop/category/daily-deals#';    
+ 
+     //wait until there are no 2 connections in 500ms
+    await page.goto(url, {waitUntil: 'networkidle2'});
+
+    var scrollTimer = page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            var totalHeight = 0
+            var distance = 600
+            var timer = setInterval(() => {
+                window.scrollBy(0, distance)
+                totalHeight += distance
+    
+                if(totalHeight >= document.body.scrollHeight){
+                    clearInterval(timer)
+                    resolve()
+                }
+            }, 200)
+        })  
+    })
+    
+    // var crawler = scrollTimer.then(async () => {
+    //     var urls = await page.evaluate(() => {
+    //         var links = [...document.querySelectorAll('div>a>img.itemImage.replace')]
+    //         return links.map(img => {
+    //             return img.src
+    //         })
+    //     })
+    
+    //     await page.close()
+    //     return Promise.resolve(urls)
+    // }).catch((e) => {
+    //     console.log(e)
+    // })
+
+    var trawl = scrollTimer.then(async ()=>{
+        var images = await page.evaluate(()=>{
+            var ret = []
+            document.querySelectorAll('div>a>img.itemImage.replace').forEach(x=>ret.push(x.src));
+            return ret;
+        })
+        await page.close()
+        return images.join('\n\n')
+    })
+    return trawl;
+}
 
 var helpTip = ()=>{
     return `/fetch`+ " to fetch the daily sales for POE MTX"
@@ -55,13 +113,30 @@ var startTip = ()=>{
     return `/fetch`+ " to fetch the daily sales for POE MTX\n"+ `/help`+" for help tooltip"
 }
 
-bot.start((ctx) => ctx.reply(startTip()))
-bot.help((ctx) => ctx.reply(helpTip()))
+bot.start((ctx) => {
+    console.log('start command recd')
+    ctx.reply(startTip())
+})
+
+bot.help((ctx) => {
+    console.log('help command recd')
+    ctx.reply(helpTip())
+})
+
 bot.on('sticker', (ctx) => ctx.reply('very nais sticcer'))
+
 bot.hears('query', (ctx) => ctx.reply('something'))
+
 bot.command('fetch', async (ctx)=>{
+    console.log('fetch command recd')
     ctx.reply('fetching latest info from website...')
     ctx.reply(await dailyList())
+})
+
+bot.command('links', async (ctx)=>{
+    console.log('fetch image links')
+    ctx.reply('fetching links from website...')
+    ctx.reply(await imageLinks())
 })
 
 //bot.launch()
